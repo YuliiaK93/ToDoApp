092bb946e56d190c3b83c5079f04073d2d9a47e6
Yuliia <yulia.karnoza@gmail.com>
2023-04-30 00:25:39 -0400

    Created Delete and Add functionalities

--- src/main/java/ToDoApp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ToDoApp.repository;
-
-import ToDoApp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-
-}
